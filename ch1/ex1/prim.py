cf501bd164b6e36bf7f5ca6b04331c5e5705b5c1
Aleksei Mavrin <al-mavrin@yandex-team.ru>
2025-01-01 21:42:25 +0300

    use logging, not print

--- ch1/ex1/prim.py
+++ ch1/ex1/prim.py
@@ -1,6 +1,10 @@
 import random
 import yaml
 import heapq
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def read_graph(file):
@@ -9,7 +13,7 @@ def read_graph(file):
             graph = yaml.safe_load(f)
         return graph
     except Exception as e:
-        print(f"cannot open {file}:", e)
+        logging.info(f"cannot open {file}: {e}")
         return None
 
 
@@ -17,22 +21,26 @@ def check_graph_symmetric(graph):
     if graph is None:
         return False
     if 'nodes' not in graph:
-        print("nodes not in graph")
+        logging.info("key 'nodes' not in graph")
         return False
     if 'edges' not in graph:
-        print("edges not in graph")
+        logging.info("key 'edges' not in graph")
         return False
+    if not graph['nodes']:
+        return True
+    if not graph['edges']:
+        return True
     for node1 in graph['nodes']:
         for node2 in graph['nodes']:
             node1_peers = graph['edges'][node1]
             node2_peers = graph['edges'][node2]
             if (node1 in node2_peers) != (node2 in node1_peers):
-                print(f"peers for {node1} {node1_peers} and {node2} {node2_peers} do not match")
+                logging.info(f"peers for {node1} {node1_peers} and {node2} {node2_peers} do not match")
                 return False
             if node1 not in node2_peers:
                 continue
             if node2_peers[node1] != node1_peers[node2]:
-                print(f"edge weights are not equal for {node1}-{node2} ({node1_peers[node2]})",
+                logging.info(f"edge weights are not equal for {node1}-{node2} ({node1_peers[node2]})",
                         f"and {node2}-{node1} ({node2_peers[node1]})")
                 return False
     return True
@@ -73,7 +81,7 @@ def find_minimal(graph):
 def main(file):
     graph = read_graph(file)
     if not check_graph_symmetric(graph):
-        print("graph is invalid or not symmetric")
+        logging.error("graph is invalid or not symmetric")
         exit(1)
     weight, minimal = find_minimal(graph)
     assert(weight == 14)
